98aeef6e5999410ab7c4b18864312d788e0d519f
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-06-03 17:15:24 -0700

    Working, making layout changes now

---
+++ script.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+	$("#deposit_checking").on("click", add_to_checking);
+	$("#deposit_saving").on("click", add_to_saving);
+	$("#withdraw_checking").on("click", withdraw_from_checking);
+	$("#withdraw_saving").on("click", withdraw_from_saving);
+
+	make_colors();
+
+	function add_to_checking(event){
+		var value = $("#text_checking").val();
+		value = parseInt(value);
+		if (isNaN(value)){
+			alert("Number entered is not a number");
+		} else {
+			total = parseInt($('#check_sum').text()) + value;
+			$('#check_sum').text(total.toString()) ;
+		}
+		event.preventDefault();
+		make_colors();
+	}
+
+	function add_to_saving(event){
+		var value = $("#text_saving").val();
+		value = parseInt(value);
+		if (isNaN(value)){
+			alert("Number entered is not a number");
+		} else {
+			total = parseInt($('#save_sum').text()) + value;
+			$('#save_sum').text(total.toString()) ;
+		}
+		event.preventDefault();
+		make_colors();
+	}
+
+	function withdraw_from_saving(event){
+		event.preventDefault();
+		var value = $("#text_saving").val();
+		if (isNaN(value)){
+			alert("Number entered is not a number");
+		} else {
+			withdraw_saving(value);
+		}
+		make_colors();
+	}
+
+
+	function withdraw_saving(value){
+		var total = parseInt($('#save_sum').text()) - value;
+		if (total < 0){
+			alert("You do not have enough money");
+			return false;
+		}else{
+			$('#save_sum').text(total.toString()) ;
+			return true;
+		}
+	}
+
+	function withdraw_from_checking(event){
+		event.preventDefault();
+		var value = $("#text_checking").val();
+		value = parseInt(value);
+		if (isNaN(value)){
+			alert("Number entered is not a number");
+		} else {
+			total = parseInt($('#check_sum').text()) - value;
+			if (total < 0){
+				if (withdraw_saving(-total)){
+					$('#check_sum').text('0') ;
+				}				
+			} else {
+				$('#check_sum').text(total.toString()) ;
+			}
+		}
+		make_colors();
+	}
+
+	function make_colors(){
+		var val1 = parseInt($('#check_sum').text());
+		switch (true){
+			case val1 < 100 :
+				$('#checking').css('background-color', 'red');
+				break;
+			case val1 < 200 : 
+				$('#checking').css('background-color', 'yellow');
+				break
+			default:
+					$('#checking').css('background-color', 'green');
+					break;
+		}
+		var val1 = parseInt($('#save_sum').text());
+		switch (true){
+			case val1 < 100 :
+				$('#saving').css('background-color', 'red');
+				break;
+			case val1 < 200 : 
+				$('#saving').css('background-color', 'yellow');
+				break
+			default:
+					$('#saving').css('background-color', 'green');
+					break;
+		}
+
+	}
+
+});
+
